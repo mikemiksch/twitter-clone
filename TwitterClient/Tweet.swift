813685cc07943517be863d479a40498d943a85dd
mikemiksch <mikemiksch@gmail.com>
2017-03-20 12:15:28 -0700

    Made User and Tweet classes.

---
+++ TwitterClient/Tweet.swift
@@ -0,0 +1,28 @@
+//
+//  Tweet.swift
+//  TwitterClient
+//
+//  Created by Mike Miksch on 3/20/17.
+//  Copyright © 2017 Mike Miksch. All rights reserved.
+//
+
+import Foundation
+
+class Tweet {
+    let text : String
+    let id : String
+    
+    let user : User?
+    
+    init?(json: [String : Any]) {
+        if let text = json["text"] as? String, let id = json["id_str"] as? String {
+            self.text = text
+            self.id = id
+            if let userDictionary = json["user"] as? [String : Any] {
+                self.user = User(json: userDictionary)
+            }
+        } else {
+            return nil
+        }
+    }
+}
